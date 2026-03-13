789929772dec1b2b3f12b87b059e6965e13186ac
guilherme39267-ux <guilherme.39267@edu.campos.rj.gov.br>
2026-03-13 16:01:40 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,52 +3,52 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const path = require('path');
+const fs = require('fs');
 
 const PORT = process.env.PORT || 3000;
 
-// Serve static files from the 'public' directory
+// Serve static files from both root and /public (works locally and on Render)
+app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/assets', express.static(path.join(__dirname))); // Expose root folder for assets (cidade.jpg, etc)
+
+// Expose root folder for assets (fundo.jpg, personagem.jpg, etc)
+app.use('/assets', express.static(__dirname));
+
+// Serve index.html for the root route
+app.get('/', (req, res) => {
+    const publicPath = path.join(__dirname, 'public', 'index.html');
+    const rootPath = path.join(__dirname, 'index.html');
+    if (fs.existsSync(publicPath)) {
+        res.sendFile(publicPath);
+    } else {
+        res.sendFile(rootPath);
+    }
+});
 
 // Store connected players
 const players = {};
 
 io.on('connection', (socket) => {
     console.log(`User connected: ${socket.id}`);
-
-    // Create a new player
-    players[socket.id] = {
-        x: 400, // Initial X position
-        y: 450, // Initial Y position
-        id: socket.id
-    };
-
-    // Send current players to the new user
+    players[socket.id] = { x: 400, y: 450, id: socket.id };
     socket.emit('currentPlayers', players);
-
-    // Notify all other users about the new player
     socket.broadcast.emit('newPlayer', players[socket.id]);
 
-    // Handle player movement
     socket.on('playerMovement', (movementData) => {
         if (players[socket.id]) {
             players[socket.id].x = movementData.x;
             players[socket.id].y = movementData.y;
-            // Broadcast the new position to everyone else
             socket.broadcast.emit('playerMoved', players[socket.id]);
         }
     });
 
-    // Handle chat messages
     socket.on('chatMessage', (msgText) => {
         io.emit('chatMessage', { id: socket.id, text: msgText });
     });
 
-    // Handle disconnection
     socket.on('disconnect', () => {
         console.log(`User disconnected: ${socket.id}`);
         delete players[socket.id];
-        // Notify others
         io.emit('playerDisconnected', socket.id);
     });
 });
